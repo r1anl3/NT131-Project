09dd4b8162005c7973fe92e7f406bcfceed97864
r1anl3 <levu1409@gmail.com>
2024-04-23 21:27:46 +0700

    add library testers

---
+++ Library/ESP8266_BynkTester/ESP8266_BynkTester.ino
@@ -0,0 +1,28 @@
+#define BLYNK_PRINT Serial
+
+/* Fill in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "YOUR_TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "YOUR_AUTH_TOKEN"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YOUR_SSID";
+char pass[] = "YOUR_PASSWORD";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
